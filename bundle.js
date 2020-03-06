d7cd045df035ecfbd1b66d86a767071967d099a6
Jun Zheng <arthurjordan@gmail.com>
2020-03-06 11:59:12 +1100

    first assignment

--- bundle.js
+++ bundle.js
@@ -1,1 +1,39 @@
-ReactDOM.render( < p > hello world < /p>, document.getElementById('app')
+const LayerContext = React.createContext();
+class Layer extends React.Component {
+  static contextType = LayerContext;
+  render() {
+    return (
+      <ul>
+        <li>{this.context.name}</li>
+        {this.context.children}
+      </ul>
+    );
+  }
+}
+const HocContext = props => (
+  <LayerContext.Provider
+    value={{
+      name: props.name,
+      children: props.children
+    }}
+  >
+    <Layer />
+  </LayerContext.Provider>
+);
+function App() {
+  return (
+    <div className="App">
+      <HocContext name="root">
+        <HocContext name="a">
+          <HocContext name="a1" />
+          <HocContext name="a2" />
+        </HocContext>
+        <HocContext name="b">
+          <HocContext name="b1" />
+          <HocContext name="b2" />
+        </HocContext>
+      </HocContext>
+    </div>
+  );
+}
+ReactDOM.render(<App />, document.getElementById("app"));
